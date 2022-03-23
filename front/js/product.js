6ed434b6a4b930f8a5a1b73702b93aee3a13a2ea
Sasdobson <Cle_7@hotmail.fr>
2022-03-23 13:02:25 +0100

    nnn

--- front/js/product.js
+++ front/js/product.js
@@ -17,11 +17,60 @@ fetch('http://localhost:3000/api/products/' + id)
         console.log(canape.imageUrl)
         console.log(canape.description)
         console.log(canape._id)
-        
+        console.log(canape.colors)
         
         var img = document.createElement("img");
         img.src = canape.imageUrl;
     
-        var div = document.getElementById("item__img").appendChild(img)
+        var div = document.getElementsByClassName('item__img')
+        [0].appendChild(img)
+        
+        var titre = document.getElementById('title')
+        titre.innerHTML = canape.name
+
+        var prix = document.getElementById('price')
+        price.innerHTML = canape.price
+
+        var description = document.getElementById('description')
+        description.innerHTML = canape.description
        
+        let select = document.getElementById('colors')
+       
+       
+    canape.colors.forEach((couleur)=> {
+    let option = document.createElement('option')
+
+    option.innerHTML=`${couleur}`
+    option.value=`${couleur}`
+    select.appendChild(option)
+           
+        });
+       
+
+var input = document.getElementById('quantity');
+var n = input.value;
+n = Number(n);
+if (n < 0) {
+    alert('Choisir quantitée entre 0-100');
+    input.value = 0;
+} else if (n > 100) {
+    alert('Choisir quantitée entre 0-100');
+    input.value = 100;
+}
+
+    let addToCart = document.getElementById("addToCart")
+    console.log(addToCart)
+    addToCart.addEventListener("click", () => {
+        let select = document.getElementById("colors")
+        console.log(select.value)
+        localStorage.setItem("canape", canape._id)
+        localStorage.setItem("canape", canape.name)
+        localStorage.setItem("canape", canape.price)
+
+    })
+   
+
+
 })
+               
+        
