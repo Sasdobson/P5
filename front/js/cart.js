c8ffe83998d94da411da5a11509f6b35e09707a1
Sasdobson <Cle_7@hotmail.fr>
2022-04-13 13:04:31 +0200

    ..

--- front/js/cart.js
+++ front/js/cart.js
@@ -30,6 +30,8 @@
         let section = document.getElementById('cart__items')
         let article = document.createElement('article')
         article.className = 'cart__item'
+        article.dataset.id = canape._id
+        article.dataset.color = ligne_de_panier.couleur
         section.appendChild(article)
 
         let Conteneur0 = document.createElement("div")
@@ -56,6 +58,7 @@
         Conteneur1.appendChild(h2)
         Conteneur1.appendChild(p)
         Conteneur1.appendChild(price)
+        
 
         let Conteneur2 = document.createElement('div')
         Conteneur2.className ="cart__item__content__settings"
@@ -64,6 +67,7 @@
         Conteneur3.className ="cart__item__content__settings__quantity"
         Conteneur2.appendChild(Conteneur3)
         let qte = document.createElement('p')
+
         let qty = document.createElement('input')
         qte.innerText = 'Qté :'
         qte.className = "cart__item__content__settings__quantity"
@@ -71,8 +75,12 @@
         qty.className = "itemQuantity"
         qty.value = ligne_de_panier.quantite
         qty.max = "100"
-        qty.min = "0"
+        qty.min = "1"
         qty.name = "itemQuantity"
+        qty.ligne_de_panier_key = key
+        totalQty += ligne_de_panier.quantite
+        qty.addEventListener("change", updateQty)
+
         Conteneur3.appendChild(qte)
         Conteneur3.appendChild(qty)
         let Suppr = document.createElement('div') 
@@ -81,21 +89,26 @@
         let Supprimer = document.createElement('p') 
         Supprimer.className ='deleteItem'
         Supprimer.innerText = "Supprimer"
+        Supprimer.ligne_de_panier_key = key
+
+         Supprimer.addEventListener('click', supprimerLigne)
+
         Suppr.appendChild(Supprimer)
 
-    
-        const input = document.querySelectorAll('input')  
-      input.forEach((input)=>{
-        input.addEventListener("change", (e) => {
-          let qty = JSON.parse(localStorage.getItem(key))
-          console.log(e.target.value)
+        qty.addEventListener('change', updateAll)
+  
+    //     const input = document.querySelectorAll('input')  
+    //   input.forEach((input)=>{
+    //     input.addEventListener("change", (e) => {
+    //       let qty = JSON.parse(localStorage.getItem(key))
+    //       console.log(e.target.value)
  
           
-        })
-      })
-      function qtyupdate() { 
-        console.log(e.target.value)
-    }
+    //     })
+    //   })
+    //   function qtyupdate() { 
+    //     console.log(e.target.value)
+    // }
     
   
     
@@ -111,46 +124,140 @@
         
             
     //  })
-
-
-
         
-
-
-
-        //  document.getElementById('cart__items').innerHTML +=
-        //  `<article class="cart__item" data-id="${canape._id}" data-couleur="${ligne_de_panier.couleur}" > 
-        //     <div class="cart__item__img">
-        //       <img src="${canape.imageUrl}" alt="${canape.altTxt}">
-        //     </div>
-        //     <div class="cart__item__content">
-        //       <div class="cart__item__content__titlePrice">
-        //         <h2>${canape.name}</h2>
-        //         <p>${ligne_de_panier.couleur}</p>
-        //         <p>${canape.price} €</p>
-        //       </div>
-        //       <div class="cart__item__content__settings">
-        //         <div class="cart__item__content__settings__quantity">
-        //           <p>Qté : </p>
-        //           <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${ligne_de_panier.quantite}">
-        //         </div>
-        //        <div class="cart__item__content__settings__delete">
-        //            <p class="deleteItem">Supprimer</p>
-        //        </div>
-        //       </div>
-        //     </div>
-        //   </article>`
+        //  totalPrice += canape.price * ligne_de_panier.quantite
+        //  totalQty += ligne_de_panier.quantite
  
- 
-         totalPrice += canape.price * ligne_de_panier.quantite
-         totalQty += ligne_de_panier.quantite
- 
-         document.getElementById('totalQuantity').innerText = totalQty
-         document.getElementById('totalPrice').innerText = totalPrice
+        //  document.getElementById('totalQuantity').innerText = totalQty
+        //  document.getElementById('totalPrice').innerText = totalPrice
  
      });
  }
- 
+
+ function updateQty(event) {
+     let input = event.target
+     let value = input.value
+     let key = input.ligne_de_panier_key;
+     // console.log(input.key)
+     //console.log(value)
+     let panier = JSON.parse(localStorage.getItem(key));
+
+     panier.quantite = parseInt(value)
+     //1 + 1 = 2
+     //"1" + 1 = "11"
+
+     localStorage.setItem(key, JSON.stringify(panier))
+     document.getElementById('totalQuantity').innerText = totalQty
+    
+     //mettre a jour le calcul et l'affichage des totaux
+ }
+ function updateAll(){
+    let key = localStorage.key(i)
+    let canape = JSON.parse(localStorage.getItem(key.price))
+    var elemsQty = document.getElementsByClassName('itemQuantity');
+    var myLength = elemsQty.length,
+    totalQty = 0;
+
+    for (var i = 0; i < myLength; ++i) {
+        totalQty += elemsQty[i].valueAsNumber;
+    }
+    
+    let productTotalQuantity = document.getElementById('totalQuantity');
+    productTotalQuantity.innerHTML = totalQty;
+    
+    totalPrice = 0;
+    for (var i = 0; i < myLength; ++i) {
+        totalPrice += (elemsQty[i].valueAsNumber * canape);
+    }
+
+    let productTotalPrice = document.getElementById('totalPrice');
+    productTotalPrice.innerHTML = totalPrice;
+}
+
+
+ function supprimerLigne(event)
+{
+    let input = event.target
+    let key = input.ligne_de_panier_key;
+    let panier = JSON.parse(localStorage.getItem(key));
+    localStorage.removeItem(key)
+    //console.log('article[data-id="'+panier.id+'"][data-color="'+panier.couleur+'"]')
+    document.querySelector('article[data-id="'+panier.id+'"][data-color="'+panier.couleur+'"]').remove()
+
+    //mettre a jour le calcul et l'affichage des totaux
+}
+function Form() {
+    
+    let form = document.querySelector(".cart__order__form");
+
+    
+    let emailRegExp = new RegExp('^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$');
+    let charRegExp = new RegExp("^[a-zA-Z ,.'-]+$");
+    let addressRegExp = new RegExp("^[0-9]{1,3}(?:(?:[,. ]){1}[-a-zA-Zàâäéèêëïîôöùûüç]+)+");
+    form.firstName.addEventListener('change', function() {
+        validFirstName(this);
+    });
+    form.lastName.addEventListener('change', function() {
+        validLastName(this);
+    });
+    form.address.addEventListener('change', function() {
+        validAddress(this);
+    });
+    form.city.addEventListener('change', function() {
+        validCity(this);
+    });
+    form.email.addEventListener('change', function() {
+        validEmail(this);
+    });
+    const validFirstName = function(inputFirstName) {
+        let firstNameErrorMsg = inputFirstName.nextElementSibling;
+
+        if (charRegExp.test(inputFirstName.value)) {
+            firstNameErrorMsg.innerHTML = '';
+        } else {
+            firstNameErrorMsg.innerHTML = 'Veuillez renseigner ce champ.';
+        }
+    };
+
+    const validLastName = function(inputLastName) {
+        let lastNameErrorMsg = inputLastName.nextElementSibling;
+
+        if (charRegExp.test(inputLastName.value)) {
+            lastNameErrorMsg.innerHTML = '';
+        } else {
+            lastNameErrorMsg.innerHTML = 'Veuillez renseigner ce champ.';
+        }
+    };
+    const validAddress = function(inputAddress) {
+        let addressErrorMsg = inputAddress.nextElementSibling;
+
+        if (addressRegExp.test(inputAddress.value)) {
+            addressErrorMsg.innerHTML = '';
+        } else {
+            addressErrorMsg.innerHTML = 'Veuillez renseigner ce champ.';
+        }
+    };
+    const validCity = function(inputCity) {
+        let cityErrorMsg = inputCity.nextElementSibling;
+
+        if (charRegExp.test(inputCity.value)) {
+            cityErrorMsg.innerHTML = '';
+        } else {
+            cityErrorMsg.innerHTML = 'Veuillez renseigner ce champ.';
+        }
+    };
+
+    const validEmail = function(inputEmail) {
+        let emailErrorMsg = inputEmail.nextElementSibling;
+
+        if (emailRegExp.test(inputEmail.value)) {
+            emailErrorMsg.innerHTML = '';
+        } else {
+            emailErrorMsg.innerHTML = 'Veuillez renseigner votre email.';
+        }
+    };
+    }
+
 //  document.getElementById('cart') //retourne un element
 //  document.getElementsByClassName() //retourne un tableau d'élement
 //  document.querySelector('article .banniere #cart') //retourne un element (le premier résultat) ou null
